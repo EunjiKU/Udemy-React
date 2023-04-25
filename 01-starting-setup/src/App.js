04a1ae270b47554d7dd941810cd7bfbb048cd8c3
KUEUNJI <yoo128@easymedia.net>
2023-04-25 17:59:32 +0900

    섹선5 - 70번 듣는중

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Expense from './components/Expenses/Expense';
 import NewExpense from './components/NewExpense/NewExpense';
