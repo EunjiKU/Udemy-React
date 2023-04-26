1137324042025647c88dd4a739d0d828c89f26a6
KUEUNJI <yoo128@easymedia.net>
2023-04-26 12:23:42 +0900

    섹션5 - 76번 과제 전 백업

--- 01-starting-setup/src/components/Expenses/Expense.js
+++ 01-starting-setup/src/components/Expenses/Expense.js
@@ -12,18 +12,31 @@ const Expense = (props) => {
     setFilteredYear(selectedYear);
   }
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No Expenses found.</p>;
+
+  if(filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
+
   return (
     <div>
       <CardItem className="expenses">
-        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} ></ExpensesFilter>
-        {props.items.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {expensesContent}
       </CardItem>
     </div>
   )
