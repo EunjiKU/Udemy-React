e7543ecdedbe8ed8147563625b666b8efc1324da
KUEUNJI <yoo128@naver.com>
2023-05-11 00:36:42 +0900

    섹션8 - 105번부터

--- 04-starting-project/01-starting-project/src/components/Users/AddUser.js
+++ 04-starting-project/01-starting-project/src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React, useState} from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import classes from './AddUser.module.css';
@@ -6,17 +6,41 @@ import classes from './AddUser.module.css';
 // ⭐ 새로운 사용자를 추가하는 로직이 있는 컴포넌트
 
 const AddUser = props => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
   const addUserHandler = (event) => {
     event.preventDefault();
+
+    if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      console.log("입력창이 비어져있습니다.");
+      return;
+    }
+    if(enteredAge < 1) {
+      console.log("Age가 1보다 작습니다.");
+      return;
+    }
+
+    console.log(enteredUsername, enteredAge);
+
+    // input 초기화
+    setEnteredUsername('');
+    setEnteredAge('');
+  }
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  }
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   }
   
   return (
-    <Card cccccc={classes.input}>
+    <Card classes={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
