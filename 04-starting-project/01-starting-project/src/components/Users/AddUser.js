5637c38aabe367361fe959f73db75608eab75332
KUEUNJI <112163447+EunJiKU8151@users.noreply.github.com>
2023-05-18 18:02:48 +0900

    섹션10 120들을차례

--- 04-starting-project/01-starting-project/src/components/Users/AddUser.js
+++ 04-starting-project/01-starting-project/src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import {React, useState} from 'react';
+import {React, useState, useRef} from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
@@ -8,58 +8,52 @@ import classes from './AddUser.module.css';
 // ⭐ 새로운 사용자를 추가하는 로직이 있는 컴포넌트
 
 const AddUser = props => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  const nameInputRef = useRef(); 
+  const ageInputRef = useRef(); 
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
 
-    if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+
+    if(enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
         title: "Invalid input",
         message: "Please enter a valid name and age (non-empty values)."
       })
-      console.log("입력창이 비어져있습니다.");
       return;
     }
-    if(+enteredAge < 1) {
+
+    if(+enteredUserAge < 1) {
       setError({ 
         title: "Invalid age",
         message: "Please enter a valid age (> 0)."
       })
-      console.log("Age가 1보다 작습니다.");
       return;
     }
 
-    console.log(enteredUsername, enteredAge);
-    props.onAddUser(enteredUsername, enteredAge);
+    props.onAddUser(enteredName, enteredUserAge);
 
-    // input 초기화
-    setEnteredUsername('');
-    setEnteredAge('');
-  }
-
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  }
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
+    // 초기화
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
   }
 
   const errorHandler = () => {
     setError(null);
   }
-  
+
   return (
     <Wrapper>
       {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}></ErrorModal>}
       <Card classes={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
-          <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
+          <input id="username" type="text" ref={nameInputRef} />
           <label htmlFor="age">Age (Years)</label>
-          <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
+          <input id="age" type="number" ref={ageInputRef} />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
