826e775323ee92bd747d631080711a84ca1937a3
KUEUNJI <112163447+EunJiKU8151@users.noreply.github.com>
2023-09-22 17:59:41 +0900

    ref하는중

--- 06-Building a Food Order App/src/components/Meals/MealItem/MealItemForm.js
+++ 06-Building a Food Order App/src/components/Meals/MealItem/MealItemForm.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css'
 
 const MealItemForm = props => {
-  return <form className={classes.form}>
-    <Input label="Amount" input={{
-      // id: 'amount',
-      id: 'amount_' + props.id,
-      type: 'number',
-      min: '1',
-      max: '5',
-      step: '1',
-      defaultValue: '1'
-    }} />
-    <button>+ Add</button>
-  </form>
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHanlder = event => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumnber = +enteredAmount;
+
+    // 값이 입력되자 않았거나, 1보다 작거나, 5보다 크거나
+    if(enteredAmount.trim().length === 0 || enteredAmountNumnber < 1 || enteredAmountNumnber > 5 ) {
+      // return해서 함수 실행 정지!!!
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumnber);
+  }
+
+  return (
+    <form className={classes.form} onSubmit={submitHanlder}>
+      <Input
+        ref={amountInputRef}
+        label="Amount"
+        input={{
+          id: 'amount',
+          // id: 'amount_' + props.id,
+          type: 'number',
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: '1'
+        }} />
+      <button>+ Add</button>
+      {!amountIsValid && <p>유효한 수량(1-5)을 입력하세요!!!</p>}
+    </form>
+  )
 }
 
 export default MealItemForm;
