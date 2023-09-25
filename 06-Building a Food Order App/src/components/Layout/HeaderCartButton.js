22792444255a2229c93a0a8445d47839423dcd31
KUEUNJI <112163447+EunJiKU8151@users.noreply.github.com>
2023-09-25 17:24:37 +0900

    장바구니 모션 추가

--- 06-Building a Food Order App/src/components/Layout/HeaderCartButton.js
+++ 06-Building a Food Order App/src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,43 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
+  const [btnIsHighLighted, setBtnIsHighLighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = cartCtx;
+
+  // const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+
+  const btnClasses = `${classes.button} ${btnIsHighLighted ? classes.bump : ''}`;
+
+  useEffect(() => {
+    // ✅ 하나 이상이 있는 경우에만 실행
+    // if(cartCtx.items.length === 0) {
+    if(items.length === 0) {
+      return;
+    }
+    setBtnIsHighLighted(true);
+    
+    const timer = setTimeout(() => {
+      setBtnIsHighLighted(false);
+    }, 300)
+
+    // ✅ useEffect에서 함수를 반환하면, 리액트에 의해 클린업 함수로 자동으로 호출된다.
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items])
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
