b2924c8ea4e3577009690cb9af192f2220146127
KUEUNJI <112163447+EunJiKU8151@users.noreply.github.com>
2023-09-26 15:58:39 +0900

    리스트

--- 07-A Look Behind The Scenes Of React, Optimization Techniques/src/App.js
+++ 07-A Look Behind The Scenes Of React, Optimization Techniques/src/App.js
@@ -6,20 +6,26 @@ import DemoOutput from './components/Demo/DemoOutput'
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log("뀨잉");
 
   const toggleParagraphHandler = useCallback(() => {
-    // setShowParagraph(!showParagraph);
-    setShowParagraph(prevShowParagraph => !prevShowParagraph);
-  }, [])
+    if(allowToggle) {
+      setShowParagraph(prevShowParagraph => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  }
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {/* {showParagraph && <p>This is new!</p>} */}
-      <DemoOutput show={false} />
-      <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Paragraph!</Button><br/><br/>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
