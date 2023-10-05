35afe863f670268e586ca8651dcad30c59f4f077
KUEUNJI <112163447+EunJiKU8151@users.noreply.github.com>
2023-10-05 15:42:15 +0900

    custmo hook name 완료

--- 11-Form/src/components/SimpleInput.js
+++ 11-Form/src/components/SimpleInput.js
@@ -1,13 +1,19 @@
 import { useState } from 'react';
+import useInput from '../hooks/use-input';
 
 const SimpleInput = (props) => {
-  const [enterdName, setEnterdName] = useState('');
-  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  const {
+    value: enterdName,
+    isValid: enteredNameIsValid,
+    hasError: nameInputHasError,
+    valueChangeHandler: nameChangeHandler,
+    valueBlurHandler: nameBlurHandler,
+    reset: resetNameInput
+  } = useInput(value => value.trim() !== '');
+
   const [enterdEmail, setEnterdEmail] = useState('');
   const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
 
-  const enteredNameIsValid = enterdName.trim() !== '';
-  const nameInputIsValid = !enteredNameIsValid && enteredNameTouched;
   const enteredEmailIsValid = enterdEmail.includes('@'); // includes : 특정 문자열을 포함하는지 확인
   const emailInputIsValid = !enteredEmailIsValid && enteredEmailTouched;
 
@@ -18,17 +24,11 @@ const SimpleInput = (props) => {
     }
 
   // ✅ 인풋 값 입력할 때마다
-  const nameInputChangeHandler = event => {
-    setEnterdName(event.target.value);
-  }
   const emailInputChangeHandler = event => {
     setEnterdEmail(event.target.value);
   }
 
   // ✅ 인풋에서 포커스 잃었을 때
-  const nameInputBlurHandler = event => {
-    setEnteredNameTouched(true);
-  }
   const emailInputBlurHandler = event => {
     setEnteredEmailTouched(true);
   }
@@ -37,22 +37,20 @@ const SimpleInput = (props) => {
   const formSubmissionHandler = event => {
     event.preventDefault();
 
-    // setEnteredNameTouched(true);
-    // setEnteredEmailTouched(true);
-
     if(!enteredNameIsValid || !enteredEmailIsValid) {
       return;
     }
 
     console.log("useState : " + enterdName);
-    setEnterdName('');
-    setEnteredNameTouched(false);
+    resetNameInput();
+    // setEnterdName('');
+    // setEnteredNameTouched(false);
     console.log("useState : " + enterdEmail);
     setEnterdEmail('');
     setEnteredEmailTouched(false);
   }
 
-  const nameInputClasses = nameInputIsValid ? 'form-control invalid' : 'form-control';
+  const nameInputClasses = nameInputHasError ? 'form-control invalid' : 'form-control';
   const emailInputClasses = emailInputIsValid ? 'form-control invalid' : 'form-control';
 
   return (
@@ -60,9 +58,9 @@ const SimpleInput = (props) => {
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input value={enterdName} type='text' id='name'
-          onChange={nameInputChangeHandler} onBlur={nameInputBlurHandler} />
+          onChange={nameChangeHandler} onBlur={nameBlurHandler} />
       </div>
-      {nameInputIsValid && <p className='error-text'>Name must not be empty.</p>}
+      {nameInputHasError && <p className='error-text'>Name must not be empty.</p>}
       <div className={emailInputClasses}>
         <label htmlFor='email'>Your E-mail</label>
         <input value={enterdEmail} type='email' id='email'
