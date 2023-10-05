5a4ed3372edc00189a4c78a803da0817e62aef3b
KUEUNJI <112163447+EunJiKU8151@users.noreply.github.com>
2023-10-05 17:46:50 +0900

    음식앱 HTTP Form 시작

--- 11-Form/src/hooks/use-input.js
+++ 11-Form/src/hooks/use-input.js
@@ -1,30 +1,47 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+const initialInputState = {
+  value: '',
+  isTouched: false
+}
+
+const inputStateReducer = (state, action) => {
+  if(action.type === 'INPUT') {
+    return { value: action.value, isTouched: state.isTouched }
+  }
+  if(action.type === 'BLUR') {
+    return { isTouched: true, value: state.value }
+  }
+  if(action.type === 'RESET') {
+    return { isTouched: false, value: '' }
+  }
+
+  return initialInputState;
+}
 
 const useInput = (validateValue) => {
-  const [enterValue, setEnterValue] = useState('');
-  const [isTouched, setIsTouched] = useState(false);
+  const [inputState, dispatch] = useReducer(inputStateReducer, initialInputState);
 
-  const valueIsValid = validateValue(enterValue);
-  const hasError = !valueIsValid && isTouched;
+  const valueIsValid = validateValue(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
 
   // ✅ 인풋 값 입력할 때마다
   const valueChangeHandler = event => {
-    setEnterValue(event.target.value);
+    dispatch({type: 'INPUT', value: event.target.value});
   }
 
   // ✅ 인풋에서 포커스 잃었을 때
   const valueBlurHandler = event => {
-    setIsTouched(true);
+    dispatch({type: 'BLUR'});
   }
 
   // ✅ 리셋
   const reset = () => {
-    setEnterValue('');
-    setIsTouched(false);
+    dispatch({type: 'RESET'});
   }
 
   return {
-    value: enterValue,
+    value: inputState.value,
     isValid: valueIsValid,
     hasError,
     valueChangeHandler,
