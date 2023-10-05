b9d3dcb1eb3efff211285ccca57c746be07f3ab6
KUEUNJI <112163447+EunJiKU8151@users.noreply.github.com>
2023-10-05 12:00:36 +0900

    전체 양식 유효성 관리 완료

--- 11-Form/src/components/SimpleInput.js
+++ 11-Form/src/components/SimpleInput.js
@@ -1,11 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const SimpleInput = (props) => {
   const [enterdName, setEnterdName] = useState('');
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  // const [formIsValid, setFormIsValid] = useState(false);
 
   const enteredNameIsValid = enterdName.trim() !== '';
   const nameInputIsValid = !enteredNameIsValid && enteredNameTouched;
+
+  let formIsValid = false;
+
+  // useEffect(() => {
+    if(enteredNameIsValid) {
+      formIsValid = true;
+    }
+    // else {
+      // setFormIsValid(false);
+    // }
+  // }, [enteredNameIsValid]);
 
   // ✅ 인풋 값 입력할 때마다
   const nameInputChangeHandler = event => {
@@ -43,7 +55,7 @@ const SimpleInput = (props) => {
       </div>
       {nameInputIsValid && <p className='error-text'>Name must not be empty.</p>}
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
