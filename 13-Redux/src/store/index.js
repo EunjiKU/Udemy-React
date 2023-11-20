9f20cb8f34c79154ea7c3d3d0aec1d17164e751f
KUEUNJI <yoo128@naver.com>
2023-11-21 01:05:13 +0900

    다중slice 디스패치

--- 13-Redux/src/store/index.js
+++ 13-Redux/src/store/index.js
@@ -1,14 +1,10 @@
-// import { createStore } from 'redux';
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const initialState = {
-  counter: 0,
-  showCounter: true
-}
+const initialCounterState = { counter: 0, showCounter: true }
 
 const counterSlice = createSlice({
   name: 'counter',
-  initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -17,7 +13,6 @@ const counterSlice = createSlice({
       state.counter--;
     },
     increase(state, action) {
-      // state.counter = state.counter + action.amount;
       state.counter = state.counter + action.payload;
     },
     toggleCounter(state) {
@@ -26,38 +21,26 @@ const counterSlice = createSlice({
   }
 });
 
-// const counterReducer = (state = initialState, action) => {
-//   if(action.type === 'increment') {
-//     return {
-//       counter: state.counter + 1,
-//       showCounter: state.showCounter
-//     }
-//   }
-//   if(action.type === 'increase') {
-//     return {
-//       counter: state.counter + action.amount,
-//       showCounter: state.showCounter
-//     }
-//   }
-//   if(action.type === 'decrement') {
-//     return {
-//       counter: state.counter - 1,
-//       showCounter: state.showCounter
-//     }
-//   }
-//   if(action.type === 'toggle') {
-//     return {
-//       counter: state.counter,
-//       showCounter: !state.showCounter
-//     }
-//   }
-//   return  state;
-// }
+
+const initialAuthState = { isAuthenticated: false }
+
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    }
+  }
+})
 
 const store = configureStore({
-  reducer: counterSlice.reducer
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer }
 });
-// const store = createStore(counterReducer);
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 export default store;
