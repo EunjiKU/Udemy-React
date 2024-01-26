9ec4aece895b95d88c60ad236e5b34712f15996b
KUEUNJI <112163447+EunJiKU8151@users.noreply.github.com>
2024-01-26 17:23:23 +0900

    [섹션20] 장바구니 연습 프로젝트

--- 13-/src/store/index.js
+++ 13-/src/store/index.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import uiSlice from './ui-slice';
-// import uiSlice from './mycart-slice';
+import cartSlice from "./cart-slice";
 
 const store = configureStore({
   reducer: {
-    ui: uiSlice.reducer
+    ui: uiSlice.reducer,
+    cart: cartSlice.reducer,
   }
 })
 
