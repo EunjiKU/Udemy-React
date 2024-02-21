88f6eb633aded2e03ce5ac97b875ddad67b1489a
EunJiKU8151 <yoo128@easymedia.net>
2024-02-21 15:55:53 +0900

    cart장바구니 리듀서, fetch - thunk

--- 14-Cart/src/App.js
+++ 14-Cart/src/App.js
@@ -1,11 +1,11 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { Fragment, useEffect, useState } from 'react';
-import { uiActions } from './store/ui-slice';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from "./components/UI/Notification";
+import { sendCartData } from './store/cart-slice';
 
 let isInitial = true;
 
@@ -16,52 +16,12 @@ function App() {
   const notification = useSelector(state => state.ui.notification);
   
   useEffect(() => {
-    const sendCartDate = async () => {
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data!'
-      }));
-      const response = await fetch('https://kej-react-default-rtdb.firebaseio.com/cart.json', {
-        method: "PUT",
-        body: JSON.stringify(cart)
-      });
-
-      // 문제 발생~~
-      if(!response.ok) {
-        // dispatch(uiActions.showNotification({
-        //   status: 'error',
-        //   title: 'Error!',
-        //   message: 'Sending cart data failed!'
-        // }));
-
-        throw new Error('Sending cart data failed!!!');
-      }
-
-      // 통과~~
-      // const responseData = await response.json();
-      // console.log(responseData);
-
-      dispatch(uiActions.showNotification({
-        status: 'success',
-        title: 'Success!',
-        message: 'Sent cart data successfully!'
-      }));
-    }
-
-    // 처음 로딩(시작) 할 때만 막기
     if(isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartDate().catch(error => {
-      dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'Error!',
-        message: 'Sending cart data failed!'
-      }));
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch])
 
   return (
