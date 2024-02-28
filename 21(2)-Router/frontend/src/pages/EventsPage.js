4f02a31c71f133f795d40218326b25bcba5be64a
EunJiKU8151 <yoo128@easymedia.net>
2024-02-28 17:05:27 +0900

    useEffect대신 useLoaderData로 백엔드에서 데이터 받아오기

--- 21(2)-Router/frontend/src/pages/EventsPage.js
+++ 21(2)-Router/frontend/src/pages/EventsPage.js
@@ -1,32 +1,54 @@
-import { Link } from "react-router-dom"
+// import { useEffect, useState } from "react"
+import { useLoaderData } from "react-router-dom";
 
-const DUMMY_EVENTS = [
-  {
-    id: 'e1',
-    title: 'Some event'
-  },
-  {
-    id: 'e2',
-    title: 'Another event'
-  },
-]
+import EventsList from "../components/EventsList";
 
 const EventsPage = () => {
+  // 📌📌📌 [App.js]의 loader로 로직 이동!!!
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [fetchedEvents, setFetchedEvents] = useState();
+  // const [error, setError] = useState();
+
+  // useEffect(() => {
+  //   async function fetchEvents() {
+  //     setIsLoading(true);
+  //     const response = await fetch('http://localhost:8080/events');
+
+  //     if (!response.ok) {
+  //       setError('Fetching events failed.');
+  //     } else {
+  //       const resData = await response.json();
+  //       setFetchedEvents(resData.events);
+  //     }
+  //     setIsLoading(false);
+  //   }
+
+  //   fetchEvents();
+  // }, []);
+
+  const events = useLoaderData();
+
   return (
     <>
-      <h1>EventsPage</h1>
-      <ul>
-        {
-          DUMMY_EVENTS.map(item => {
-            return <li key={item.id}>
-              {/* <Link to={`/events/${item.id}`}>{item.title}</Link> */}
-              <Link to={item.id}>{item.title}</Link>
-            </li>
-          })
-        }
-      </ul>
+      {/* <div style={{ textAlign: 'center' }}>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+      </div>
+      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents}/>} */}
+
+      {<EventsList events={events}/>}
     </>
   )
 }
 
 export default EventsPage
+
+export async function loader() {
+  const response = await fetch('http://localhost:8080/events');
+
+  if (!response.ok) {
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
