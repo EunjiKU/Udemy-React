436d08acc1a0ab61c8c3f3013598dfdf7f0694fe
EunJiKU8151 <yoo128@easymedia.net>
2024-02-28 12:17:25 +0900

    21 동적라우팅

--- 21-Router/src/App.js
+++ 21-Router/src/App.js
@@ -2,16 +2,20 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import RootLayout from "./pages/Root";
 import Homepage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+import ProductDetail from './pages/ProductDetail';
+import ErrorPage from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout/>,
+    errorElement: <ErrorPage />,
     children: [
       {path: '/', element: <Homepage />},
       {path: '/products', element: <ProductsPage />},
+      {path: '/products-detail/:productId', element: <ProductDetail />}
     ]
-  }
+  },
 ])
 
 function App() {
